9fe44528fa073809cd6e624483e924796cec8d88
MrAksel <mraksel@hotmail.no>
2016-01-09 16:50:26 +0100

    :))

--- Assets/Scripts/PlayerWeapon.cs
+++ Assets/Scripts/PlayerWeapon.cs
@@ -5,8 +5,7 @@ public class PlayerWeapon : MonoBehaviour
 {
 
     public string weaponName = "Basic Gun";
-
-    public int damage = 10;
+    
     public int range = 2; //seconds
     public int muzzleVelocity = 500;
 
