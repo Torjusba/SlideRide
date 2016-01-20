29eb4ac6effb54de413f017d9ebefe0c578ed91b
MrAksel <mraksel@hotmail.no>
2016-01-20 15:12:20 +0100

    Simplification and formatting
    Don't need a coroutine for everything ;)

--- Assets/Scripts/KillMessageScript.cs
+++ Assets/Scripts/KillMessageScript.cs
@@ -2,22 +2,17 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class KillMessageScript : MonoBehaviour {
-    
+public class KillMessageScript : MonoBehaviour
+{
+
     public Player owner;
     public Player target;
 
-	void Start () {
+    void Start()
+    {
         Text killMessage = gameObject.GetComponentInChildren<Text>();
         killMessage.text = owner.name + " has killed " + target.name;
 
-        StartCoroutine(DeleteTimer());
-	}	
-
-    IEnumerator DeleteTimer()
-    {
-        yield return new WaitForSeconds(3f);
-        Destroy(gameObject);
-
+        Destroy(gameObject, 3f);
     }
 }
