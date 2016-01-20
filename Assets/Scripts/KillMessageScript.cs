271d1595f21a7f2e8bd754e174c3fd807a2f85b3
Torjusba <torjus.bakkene@gmail.com>
2016-01-20 14:54:01 +0100

    Initial Commit
    - Added owner information to bullets
    - Added death messages
    - Fixed issue with players killing themselves

---
+++ Assets/Scripts/KillMessageScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class KillMessageScript : MonoBehaviour {
+    
+    public Player owner;
+    public Player target;
+
+	void Start () {
+        Text killMessage = gameObject.GetComponentInChildren<Text>();
+        killMessage.text = owner.name + " has killed " + target.name;
+
+        StartCoroutine(DeleteTimer());
+	}	
+
+    IEnumerator DeleteTimer()
+    {
+        yield return new WaitForSeconds(3f);
+        Destroy(gameObject);
+
+    }
+}
